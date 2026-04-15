fb5b45f526f1ec4cdc545af46f36b43cc74cd561
karolgonzalezb <kagonzalezb@unal.edu.co>
2026-04-15 16:17:41 -0500

    Remove file handling and add print statement

--- main.py
+++ main.py
@@ -1,11 +1,1 @@
-try:
-    archivo = open("videojuegos50registros.csv", "r", encoding="utf-8")
-    print("Archivo abierto correctamente\n")
-
-    for i in range(5):
-        print(archivo.readline())
-
-    archivo.close()
-
-except:
-    print("Error: no se pudo abrir el archivo")
+prin("prueba")

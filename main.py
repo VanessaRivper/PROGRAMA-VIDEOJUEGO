26fba80bfcfa4926149c9b9d5a326c8b8af43354
karolgonzalezb <kagonzalezb@unal.edu.co>
2026-04-15 16:00:22 -0500

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-try:
-  archivo = open("videojuegos50registros.csv", "r", encoding = "utf-8")
-  print(Archivo abierto)
-  for i in range(5):
-    print(archivo.readline())
-    archivo.close()
-    

71f5dc1c0fbb08d2a1a71ff27acc55a343683729
SofiaTorres5 <dsofiatorres05@gmail.com>
2026-05-06 18:09:38 -0500

    Improve print formatting in Analisis.py
    Refactor print statements for better readability and maintainability.

--- Analisis.py
+++ Analisis.py
@@ -1,5 +1,7 @@
 def buscar(datos, termino):
     contador = 0
+    resultados = []
+    
     for fila in datos:
         texto = ",".join(fila).lower()
         if termino.lower() in texto:
@@ -8,32 +10,58 @@ def buscar(datos, termino):
                 plataforma = fila[1]
                 genero = fila[2]
                 ventas = fila[6]
-                print("Juego:", nombre, "| Plataforma:", plataforma, "| Ventas:", ventas)
+                
+                print("Juego:", nombre, 
+                      "| Plataforma:", plataforma, 
+                      "| Ventas:", ventas)
+                
                 contador += 1
+                resultados.append(fila)
+                
             except:
                 continue
+                
     print("\nSe encontraron", contador, "resultados")
+    
+    return resultados
+
+
 def estadisticas(datos):
     total = len(datos)
     suma = 0
+    
     for fila in datos:
         try:
             suma += float(fila[6])
         except:
             continue
+            
     promedio = suma / total
     print(f"\nTotal de videojuegos: {total}")
     print(f"Ventas totales: {round(suma,2)} millones")
     print(f"Promedio de ventas: {round(promedio,2)} millones")
+
+
 def filtrar(datos):
-     print("Consolas: PS3, PS4, PS2, X360, XOne, PC, PSP, Wii")
+    
+    print("Consolas: PS3, PS4, PS2, X360, XOne, PC, PSP, Wii")
+    
     p = input("De las anteriores consolas ingresa la quieras filtrar: ").strip()
+    
     contador = 0
+    resultados = []
+    
     for fila in datos:
         if p.lower() in fila[1].lower():
-            print("Juego:", fila[0], "| Plataforma:", fila[1], "| Ventas:", fila[6])
+            print("Juego:", fila[0], 
+                  "| Plataforma:", fila[1], 
+                  "| Ventas:", fila[6])
+            
             contador += 1
+            resultados.append(fila)
+
     if contador == 0:
          print(f"No se encontraron juegos para la plataforma '{p}'.")
     else:
          print(f"\nTotal encontrado: {contador} registros.")
+    return resultados
